625282117af108ad56cd68cd06f464c317a68850
Theoneandonlyajcool <ac1106508@gmail.com>
2025-10-31 07:14:30 +0100

    Fixed the issue on pr-41

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -7,7 +7,6 @@ import SignUpCategory from "../Pages/Auth/SignUpCategory";
 import VerifyEmail from "../Pages/Auth/VerifyEmail";
 import ForgetPassword from "../Pages/Auth/ForgetPassword";
 import GetStarted from "../Pages/Auth/GetStarted";
-import GetStarted2 from "../Pages/Auth/GetStarted2";
 import Landing from "../Pages/Landing";
 import ResetPassword from "../Pages/Auth/ResetPassword";
 import Sevendayfree from "../Pages/Auth/Sevendayfree";
@@ -37,8 +36,10 @@ const Router = () => {
           <Route path="/category" element={<SignUpCategory />} />
           <Route path="/verification" element={<VerifyEmail />} />
           <Route path="/forget_password" element={<ForgetPassword />} />
-          <Route path="/businessCategory" element={<GetStarted />}></Route>
-          <Route path="/businessCategory2" element={<GetStarted2 />}></Route>
+
+          <Route path="/businessCategory" element={<GetStarted />} />
+
+          <Route path="/businessCategory2" element={<GetStarted2 />} />
           <Route path="/reset_password" element={<ResetPassword />} />
           <Route path="/Sevenday_free" element={<Sevendayfree />} />
           <Route path="/queue_form" element={<QueueForm />} />
