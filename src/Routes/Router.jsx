4899b53f8873475444cf74b7dde3cb8cfa8a4fe1
Theoneandonlyajcool <ac1106508@gmail.com>
2025-11-20 02:38:30 +0100

    Implemented the privte routing

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -40,6 +40,7 @@ import MultiStepModal from "../Pages/CustomModal/CustomModal";
 import CustomStep1 from "../Pages/CustomModal/CustomStep1";
 import CustomStep2 from "../Pages/CustomModal/CustomStep2";
 import FinalStep from "../Pages/CustomModal/FinalStep";
+import Private from "../Pages/Private";
 
 // Hooks
 import { useState } from "react";
@@ -60,58 +61,71 @@ const Router = () => {
             <Route path="/sign_up/" element={<Signup />} />
             <Route path="/sign_in/" element={<SignIn />} />
             <Route path="/category" element={<SignUpCategory />} />
-            <Route path="/verification" element={<VerifyEmail />} />
             <Route path="/forget_password" element={<ForgetPassword />} />
-
-            <Route path="/businessCategory" element={<GetStarted />} />
             <Route path="/Branch_SigninV" element={<BranchSigninV />} />
+
+            <Route path="/businessCategory2" element={<GetStarted2 />} />
+            <Route path="/businessCategory" element={<GetStarted />} />
+            <Route path="/users_form/:queue/:id" element={<QueueForm />} />
             <Route
               path="/verification_forgetpassword"
               element={<VerifyForgetpassword />}
             />
-
-            <Route path="/businessCategory2" element={<GetStarted2 />} />
+            <Route path="/verification" element={<VerifyEmail />} />
             <Route path="/reset_password" element={<ResetPassword />} />
-            <Route path="/Sevenday_free" element={<Sevendayfree />} />
-            <Route path="/users_form/:queue/:id" element={<QueueForm />} />
-            <Route path="/queue_success" element={<QueueSuccess />} />
-            <Route path="/pricing" element={<Pricepage />} />
             <Route path="/singleBranch" element={<LoginForm />} />
-            <Route path="/admin_landingpage" element={<AdminLandingPage />} />
+            <Route path="/Sevenday_free" element={<Sevendayfree />} />
             <Route path="/businessCategory2" element={<GetStarted2 />} />
-            <Route path="/reset_password" element={<ResetPassword />} />
-            <Route path="/Sevenday_free" element={<Sevendayfree />} />
-            <Route path="/queue_form" element={<QueueFormNorm />} />
             <Route path="/queue_success" element={<QueueSuccess />} />
-            <Route path="/edit_branch" element={<Editbranch />} />
-            <Route path="/pricing" element={<Pricepage />} />
             <Route path="/singleBranch" element={<LoginForm />} />
-            <Route path="/admin_landingpage" element={<AdminLandingPage />} />
-            <Route path="/orgform" element={<Orgform />} />
-            <Route path="/branch" element={<Branch />} />
-            <Route path="/salesteam_modal" element={<SalesTeamModal />} />
-            <Route path="/Interest_Modal" element={<InterestModal />} />
-            <Route path="/Custom_path" element={<MultiStepModal />} />
-            <Route path="/Custom_step1" element={<CustomStep1 />} />
-            <Route path="/Custom_step2" element={<CustomStep2 />} />
-            <Route path="/Final_step" element={<FinalStep />} />
 
-            <Route path="/organization_onboarding" element={<Organization />} />
-            <Route path="/branch_onboarding" element={<BranchOnboarding />} />
-            <Route path="/review" element={<Review />} />
-            <Route path="/submitted" element={<Submitted />} />
-            <Route path="/payment_succesful/:id" element={<ThankYouModal />} />
+            {/* Privates' */}
 
-            <Route path="/dashboard/*" element={<LayoutShell />}>
-              <Route index element={<Navigate to="overview" />} />
-              <Route path="overview" element={<BranchOverview />} />
-              <Route path="allbranchview" element={<Allbranch />} />
-              <Route path="analytics" element={<Analytics />} />
-              <Route path="branch-management" element={<BranchManagement />} />
-              <Route path="settings" element={<OrganizationSettings />} />
+            <Route element={<Private />}>
+              <Route path="/pricing" element={<Pricepage />} />
+              <Route path="/admin_landingpage" element={<AdminLandingPage />} />
+              <Route path="/reset_password" element={<ResetPassword />} />
+              <Route path="/Sevenday_free" element={<Sevendayfree />} />
+              <Route path="/queue_form" element={<QueueFormNorm />} />
+              <Route path="/queue_success" element={<QueueSuccess />} />
+              <Route path="/edit_branch" element={<Editbranch />} />
+              <Route path="/pricing" element={<Pricepage />} />
+              <Route path="/admin_landingpage" element={<AdminLandingPage />} />
+              <Route path="/orgform" element={<Orgform />} />
+              <Route path="/branch" element={<Branch />} />
+              <Route path="/salesteam_modal" element={<SalesTeamModal />} />
+              <Route path="/Interest_Modal" element={<InterestModal />} />
+              <Route path="/Custom_path" element={<MultiStepModal />} />
+              <Route path="/Custom_step1" element={<CustomStep1 />} />
+              <Route path="/Custom_step2" element={<CustomStep2 />} />
+              <Route path="/Final_step" element={<FinalStep />} />
+
+              <Route
+                path="/organization_onboarding"
+                element={<Organization />}
+              />
+              <Route path="/branch_onboarding" element={<BranchOnboarding />} />
+              <Route path="/review" element={<Review />} />
+              <Route path="/submitted" element={<Submitted />} />
+              <Route
+                path="/payment_succesful/:id"
+                element={<ThankYouModal />}
+              />
+
+              <Route path="/dashboard/*" element={<LayoutShell />}>
+                <Route index element={<Navigate to="overview" />} />
+                <Route path="overview" element={<BranchOverview />} />
+                <Route path="allbranchview" element={<Allbranch />} />
+                <Route path="analytics" element={<Analytics />} />
+                <Route
+                  path="branch-management"
+                  element={<BranchManagement />}
+                />
+                <Route path="settings" element={<OrganizationSettings />} />
+              </Route>
+
+              <Route path="/admin_dashboard" element={<AdminDash />} />
             </Route>
-
-            <Route path="/admin_dashboard" element={<AdminDash />} />
           </Routes>
         </HashRouter>
       </Role.Provider>
